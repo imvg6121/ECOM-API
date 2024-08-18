cbe1df6dbcfd93b9509382819f129eda888ef89d
gauravvishal264 <imvishalgaurav264@gmail.com>
2024-08-18 13:13:08 +0530

    signin modified

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -18,46 +18,104 @@ export default class UserController{
         return res.status(400).send("Something went wrong");
     }
     }
-    async signUp(req,res,next){
-        try{
-            const {name,email,password,type} = req.body
-            //const hashedPassword=await bcrypt.hash(password,12)
-            const user =new UserModel(name,email,password,type)
-            await this.userRepository.signUp(user)
-            res.status(201).send(user)
-        }catch(err){
-            next(err)
+    // async signUp(req,res,next){
+    //     try{
+    //         const {name,email,password,type} = req.body
+    //         //const hashedPassword=await bcrypt.hash(password,12)
+    //         const user =new UserModel(name,email,password,type)
+    //         await this.userRepository.signUp(user)
+    //         res.status(201).send(user)
+    //     }catch(err){
+    //         next(err)
+    //     }
+    // }
+
+    async signUp(req, res, next) {
+        try {
+            const { name, email, password, type } = req.body;
+            
+            // Hash the password
+            const hashedPassword = await bcrypt.hash(password, 12);
+
+            // Create a new user object with the hashed password
+            const user = new UserModel(name, email, hashedPassword, type);
+
+            // Save the user to the repository
+            await this.userRepository.signUp(user);
+
+            // Send a success response
+            res.status(201).send(user);
+        } catch (err) {
+            // Pass the error to the error handling middleware
+            next(err);
         }
     }
-    async signIn(req,res){
-        try{
-            //1.find user by email
-        const user=await this.userRepository.findByEmail(req.body.email)
-        if(!user){
-            return res.status(400).send("Incorrect Credentials")
-        }else{
-            //compare password with hashedPassword
-            const result= await bcrypt.compare(req.body.password,user.password)
-            if(result){
-                //1.Create a token
-            const token = jwt.sign({
-                userId : user._id,
-                userEmail : user.email
-            },
-            process.env.JWT_SECRET,
-            {
-                expiresIn:"1h"
-            }
-            )
-            //2.Send Token
-            return res.status(200).send(token)
-            }else{
-                return res.status(400).send("Incorrect Credentials")
-            }
+
+
+    // async signIn(req,res){
+    //     try{
+    //         //1.find user by email
+    //     const user=await this.userRepository.findByEmail(req.body.email)
+    //     if(!user){
+    //         return res.status(400).send("incorrect Credentials")
+    //     }else{
+    //         //compare password with hashedPassword
+    //         const result= await bcrypt.compare(req.body.password,user.password)
+    //         if(result){
+    //             //1.Create a token
+    //         const token = jwt.sign({
+    //             userId : user._id,
+    //             userEmail : user.email
+    //         },
+    //         process.env.JWT_SECRET,
+    //         {
+    //             expiresIn:"1h"
+    //         }
+    //         )
+    //         //2.Send Token
+    //         return res.status(200).send(token)
+    //         }else{
+    //             return res.status(400).send("Incorrect Credentials")
+    //         }
+    //     }
+    // }catch(err){
+    //     console.log(err)
+    //     return res.status(200).send("Something went wrong")
+    // }
+    // }
+
+
+async signIn(req, res) {
+    try {
+        // 1. Find user by email
+        const user = await this.userRepository.findByEmail(req.body.email);
+        if (!user) {
+            return res.status(400).send("Incorrect credentials");
         }
-    }catch(err){
-        console.log(err)
-        return res.status(200).send("Something went wrong")
+
+        // 2. Compare password with hashedPassword
+        const isMatch = await bcrypt.compare(req.body.password, user.password);
+        if (isMatch) {
+            // 3. Create a token
+            const token = jwt.sign(
+                {
+                    userId: user._id,
+                    userEmail: user.email,
+                },
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: "1h",
+                }
+            );
+
+            // 4. Send Token
+            return res.status(200).json({ token });
+        } else {
+            return res.status(400).send("Incorrect credentials");
+        }
+    } catch (err) {
+        console.error(err); // Use console.error for logging errors
+        // return res.status(500).send("Something went wrong"); // Use 500 for server errors
     }
-    }
+}
 }
--- src/features/user/user.schema.js
+++ src/features/user/user.schema.js
@@ -1,17 +1,37 @@
 import mongoose from "mongoose";
 
+// export const userSchema = new mongoose.Schema({
+//     name:{type:String,maxLength:[25,"Name Cant be greater than 25 characters"]},
+//     email:{type:String,unique:true,required:true,
+//         match:[/.+\@.+\../,"Please enter a valid email"]
+//     },
+//     password:{type:String,
+//         validate:{
+//             validator:function(value){
+//                 return /^(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,12}$/.test(value)
+//             },
+//             message:"Password should be between 8 to 12 characters and contain a special character"
+//         }
+//     },
+//     type:{type:String,enum:['Customer','Seller']}
+// })import mongoose from "mongoose";
+
 export const userSchema = new mongoose.Schema({
-    name:{type:String,maxLength:[25,"Name Cant be greater than 25 characters"]},
-    email:{type:String,unique:true,required:true,
-        match:[/.+\@.+\../,"Please enter a valid email"]
+    name: {
+        type: String,
+        maxLength: [25, "Name cannot be greater than 25 characters"]
     },
-    password:{type:String,
-        validate:{
-            validator:function(value){
-                return /^(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,12}$/.test(value)
-            },
-            message:"Password should be between 8 to 12 characters and contain a special character"
-        }
+    email: {
+        type: String,
+        unique: true,
+        required: [true, "Email is required"] // Custom error message for missing email
     },
-    type:{type:String,enum:['Customer','Seller']}
-})
+    password: {
+        type: String,
+        required: [true, "Password is required"] // Custom error message for missing password
+    },
+    type: {
+        type: String,
+        enum: ['Customer', 'Seller']
+    }
+});
